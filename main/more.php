67c8d4333a173937fdc620dcbed5ee4b7466ec4e
tejasshri <tejasshrishekhar08@gmail.com>
2024-03-08 09:57:18 +0530

    update

--- main/more.php
+++ main/more.php
@@ -117,17 +117,18 @@
         <?php for ($i = 0; $i < 8; $i++) : ?>
             <div class="accordion" id="accordionExample<?php echo $i; ?>">
                 <div class="accordion-item">
-                    <h2 class="accordion-header" id="headingOne<?php echo $i; ?>">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne<?php echo $i; ?>" aria-expanded="true" aria-controls="collapseOne">
-                            Accordion Item #1
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo<?php echo $i; ?>" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                            Accordion Item #2
                         </button>
                     </h2>
-                    <div id="collapseOne<?php echo $i; ?>" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div id="panelsStayOpen-collapseTwo<?php echo $i; ?>" class="accordion-collapse collapse">
                         <div class="accordion-body">
-                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
                         </div>
                     </div>
                 </div>
+
             </div>
         <?php endfor;  ?>
     </div>
